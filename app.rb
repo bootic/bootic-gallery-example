642fef8ce47f36c2c292481ac6d3faaa7507d78e
Ismael Celis <ismaelct@gmail.com>
2014-05-22 14:54:42 +0100

    Demo app

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'bundler/setup'
+require 'sinatra/base'
+require 'hyperresource'
+require "rack/cache"
+
+class BooticAPI < HyperResource
+  self.root = 'https://api.bootic.net/v1'
+  self.headers = {
+    'Accept' => 'application/json',
+    'Authorization' => "Bearer #{ENV['BOOTIC_ACCESS_TOKEN']}"
+  }
+  
+end
+
+class App < Sinatra::Base
+  use Rack::Cache
+
+  before do
+    cache_control :public, :must_revalidate, max_age: 3600
+  end
+
+  def client
+    @client ||= BooticAPI.new.get
+  end
+
+  get '/:shop_subdomain' do |subdomain|
+    @products = client.products.where(
+      shop_subdomains: subdomain, 
+      per_page: 28,
+      sort: 'updated_on.desc', 
+      q: params[:q]
+    ).get
+
+    erb :index
+  end
+end
